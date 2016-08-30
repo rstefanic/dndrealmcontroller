7ed770d85b40bf472b2fc7df285c183ac7a6cfe7
Robert Stefanic <rstefanic72@gmail.com>
2016-08-30 17:28:22 -0500

    added trinkets script to db_scripts

---
+++ dbscripts/trinkets.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `trinkets`;
+CREATE TABLE trinkets (id INTEGER AUTO_INCREMENT, trinket VARCHAR(120), PRIMARY KEY (id));
+
+INSERT INTO trinkets (trinket) VALUES ("A mummified goblin hand"),
+  ("A piece of crystal that faintly glows in the moonlight"),
+  ("A gold coin minted in an unkown land"),
+  ("A diary written in a language you don't know"),
+  ("A brass ring that never tarnishes"),
+  ("An old chess piece made from glass"),
+  ("A pair of knucklebone dice, each with a skull symbol on the side that would normally show size pips"),
+  ("A small idol depicting a nightmarish creature that gives you unsettling dreams when you sleep near it"),
+  ("A rope necklace from which dangles four mummified elf fingers"),
+  ("The deed for a parcel of land in a realm unknown to you"),
+  
